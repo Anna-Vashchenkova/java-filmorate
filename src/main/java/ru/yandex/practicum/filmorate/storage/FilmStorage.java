8a43cdad201707681f19e175c98696ec55c6ee14
Anna Vashchenkova <anna.inkognito7@yandex.ru>
2023-11-16 23:50:48 +0300

    Исправление ошибок по ревью.

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -7,8 +7,12 @@ import java.util.Set;
 
 public interface FilmStorage {
     Film create(Film film);
+
     Film updateFilm(Film film);
+
     Set<Film> findAll();
+
     void deleteFilm(Film film);
+
     Optional<Film> getById(int filmId);
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -7,9 +7,14 @@ import java.util.Set;
 
 public interface UserStorage {
     User create(User user);
+
     User updateUser(User user);
+
     Set<User> getUsers();
+
     void deleteUser(User user);
+
     Optional<User> getUserById(int userId);
+
     Optional<User> getByEmail(String email);
     }
